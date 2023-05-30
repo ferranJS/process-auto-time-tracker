a8f656b70d78e3a52fd93ee47b6f45082a5bdb27
Ferran <ferranramirezmarti@gmail.com>
2023-05-30 14:44:29 +0200

    no definition for DeserializeObject hmm

--- Processes Auto Time Tracker/Program.cs
+++ Processes Auto Time Tracker/Program.cs
@@ -1,19 +1,40 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 public class ProcessesAutoTimeTracker {
     static Process process;
+    static TimeSpan totalTime = new TimeSpan();
+
     static void Main(string[] args) {
-        while (process == null) {
-            Console.WriteLine(process);
-            Thread.Sleep(3000);
-            process = Process.GetProcessesByName("Notepad").FirstOrDefault();
+        Console.WriteLine("Runnin'!");
+        try {
+            new StreamReader("time.json");
+        } catch (Exception ex) {
+            File.WriteAllText("time.json", "{}");
         }
-        process.EnableRaisingEvents = true;
-        process.Exited += Process_Exited;
-        process.WaitForExit();
+        using (StreamReader r = new StreamReader("time.json")) {
+            string jsonString = r.ReadToEnd();
+            dynamic json = JsonConverter.DeserializeObject(jsonString);
+            Console.WriteLine(json);
+            while (true) {
+                if (process == null) {
+                    Console.Write(".");
+                    process = Process.GetProcessesByName("SnippingTool").FirstOrDefault();
+                } else {
+                    Console.WriteLine(process);
+                    process.EnableRaisingEvents = true;
+                    process.Exited += Process_Exited;
+                    totalTime += TimeSpan.FromSeconds(3);
+                    json.
+                    Console.WriteLine(json);
+                }
+                Thread.Sleep(500);
+            }
+        }
     }
     private static void Process_Exited(object sender, EventArgs e) {
-        Process process = (Process)sender;
-        Console.WriteLine(process.ExitTime - process.StartTime);
+        Process process_ = (Process)sender;
+        process = null;
     }
 }
