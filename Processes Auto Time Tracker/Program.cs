60c9a08aa78b5de21b1e5f4209c2a32770174409
Ferran <ferranramirezmarti@gmail.com>
2023-05-30 13:02:39 +0200

    Agregar archivos de proyecto.

---
+++ Processes Auto Time Tracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+public class ProcessesAutoTimeTracker {
+    static Process process;
+    static void Main(string[] args) {
+        while (process == null) {
+            Console.WriteLine(process);
+            Thread.Sleep(3000);
+            process = Process.GetProcessesByName("Notepad").FirstOrDefault();
+        }
+        process.EnableRaisingEvents = true;
+        process.Exited += Process_Exited;
+        process.WaitForExit();
+    }
+    private static void Process_Exited(object sender, EventArgs e) {
+        Process process = (Process)sender;
+        Console.WriteLine(process.ExitTime - process.StartTime);
+    }
+}
