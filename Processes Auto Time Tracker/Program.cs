4100f72d2f160e37b9a70e13a0126d58e6ab4927
Ferran <ferranramirezmarti@gmail.com>
2023-05-30 15:10:38 +0200

    realtime time counter registering

--- Processes Auto Time Tracker/Program.cs
+++ Processes Auto Time Tracker/Program.cs
@@ -2,18 +2,35 @@
 
 public class ProcessesAutoTimeTracker {
     static Process process;
+    static TimeSpan totalTime = new TimeSpan();
+    static int sleepMs = 500;
+
     static void Main(string[] args) {
-        while (process == null) {
-            Console.WriteLine(process);
-            Thread.Sleep(3000);
-            process = Process.GetProcessesByName("Notepad").FirstOrDefault();
+        Console.WriteLine("Runnin'!");
+        using (FileStream fs = new FileStream("timeCounters.json", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read))
+        using (StreamReader sr = new StreamReader(fs))
+        using (StreamWriter sw = new StreamWriter(fs))
+        {
+            while (true) {
+                if (process == null) {
+                    Console.Write(".");
+                    process = Process.GetProcessesByName("SnippingTool").FirstOrDefault();
+                }
+                else {
+                    Console.Write("!");
+                    process.EnableRaisingEvents = true;
+                    process.Exited += Process_Exited;
+                    totalTime += TimeSpan.FromSeconds(sleepMs/1000);
+                    sw.Write("zelda: "+totalTime);
+                    // sw.Close(); without Close(), text is never written in drive
+                }
+                Console.WriteLine(sr.ReadToEnd());
+                Thread.Sleep(sleepMs);
+            }
         }
-        process.EnableRaisingEvents = true;
-        process.Exited += Process_Exited;
-        process.WaitForExit();
     }
     private static void Process_Exited(object sender, EventArgs e) {
-        Process process = (Process)sender;
-        Console.WriteLine(process.ExitTime - process.StartTime);
+        Process process_ = (Process)sender;
+        process = null;
     }
 }
