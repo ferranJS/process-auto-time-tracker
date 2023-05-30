757b38caa2fd43d859ed144616b30498d3b51bb0
Ferran <ferranramirezmarti@gmail.com>
2023-05-30 19:34:53 +0200

    cannot read after write -> flush

--- Processes Auto Time Tracker/Program.cs
+++ Processes Auto Time Tracker/Program.cs
@@ -3,28 +3,37 @@
 public class ProcessesAutoTimeTracker {
     static Process process;
     static TimeSpan totalTime = new TimeSpan();
-    static int sleepMs = 500;
-
+    static int sleepMs = 1000;
+    static StreamWriter sw;
     static void Main(string[] args) {
         Console.WriteLine("Runnin'!");
         using (FileStream fs = new FileStream("timeCounters.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read))
         using (StreamReader sr = new StreamReader(fs))
-        using (StreamWriter sw = new StreamWriter(fs))
+        using (sw = new StreamWriter(fs))
         {
+            sw.AutoFlush = true;
+            Console.WriteLine(sr.ReadToEnd());
+            if (sr.ReadToEnd() == "") {
+                sw.Write("zelda: " + totalTime, false);
+                //fs.Position = 0;
+                //fs.Seek(2, SeekOrigin.Begin);
+            }
+            Console.WriteLine(sr.ReadToEnd());
+            //Console.Write(fs.Seek(0, SeekOrigin.Begin));
+            //string time = sr.ReadToEnd().Split(' ')[0];
             while (true) {
                 if (process == null) {
                     Console.Write(".");
-                    process = Process.GetProcessesByName("SnippingTool").FirstOrDefault();
+                    process = Process.GetProcessesByName("yuzu").FirstOrDefault();
                 }
                 else {
                     Console.Write("!");
                     process.EnableRaisingEvents = true;
                     process.Exited += Process_Exited;
                     totalTime += TimeSpan.FromSeconds(sleepMs/1000);
-                    sw.Write("zelda: "+totalTime);
+                    sw.WriteLine("zelda: "+totalTime, false);
                     // sw.Close(); without Close(), text is never written in drive
                 }
-                Console.WriteLine(sr.ReadToEnd());
                 Thread.Sleep(sleepMs);
             }
         }
